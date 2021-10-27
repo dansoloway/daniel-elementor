41e1008489305edd570e4d55ac2083b8ac8dc98c
Daniel <daniel-new2@Daniel-Hadash.lan>
2021-10-27 10:37:36 +0300

    part2

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'enqueue_styles' );
+function enqueue_styles() {
+    wp_enqueue_style('parent-theme', get_template_directory_uri() .'/style.css');
+    wp_enqueue_style('child-theme', get_stylesheet_directory_uri() .'/style.css');
+
+}
+
+add_action( 'admin_enqueue_scripts', 'enqueue_admin_scripts' );
+function enqueue_admin_scripts(){
+    wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/custom-scripts.js', array('jquery'), null, false );
+    wp_localize_script( 'custom-js', 'MyAjaxObj', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) , 'placeholderimg' => get_stylesheet_directory_uri() . '/img/placeholder.png' ) );
+
+    wp_enqueue_style( 'admin-css', get_stylesheet_directory_uri() . '/css/admin.css');
+}
+
+function dumpData($data, $die = false){
+    echo "<pre>";
+    var_dump($data);
+    echo "</pre>";
+    if($die) die("Output Stopped");
+}
